2c6afb8a19e52bd98c6ec100f1b307ccb777aafe
Heera T Mohan <heeratmohan95@gmail.com>
2023-02-20 23:05:53 +0530

    Add NgBootstrap, json-server and ngrx
    * Update eslint
    * Add ngrx effects, entity, store-devtools etc
    * Add uuid
    * Add json-server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { StoreModule } from '@ngrx/store'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    StoreModule.forRoot({}, {})
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
